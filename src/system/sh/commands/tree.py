c2d03329542f1d95b52f3df6a2572b45fdb766e9
mumeimumei00 <mumeimumei0@gmail.com>
2023-09-05 16:38:43 +0900

    implementing rich's tree

--- src/system/sh/commands/tree.py
+++ src/system/sh/commands/tree.py
@@ -4,7 +4,7 @@ from system.core.file import File, DotFile
 from typing import Optional
 from rich.tree import Tree
 from rich.text import Text
-
+from rich import print
 
 class tree(Command):
     """
@@ -27,29 +27,24 @@ class tree(Command):
         else: folder = self.sys.fs.disk.current.find(name = args.get(0))
         if not folder or not isinstance(folder, Folder): return self.sys.io.display.error(f"Folder not found: {args.get(0)}")
 
-        self._tree(folder, depth = 0)
+        self._tree(folder)
 
     # TODO: Ignore dotfiles & dotfolders
     # TODO: Print the hex_addr attribute of each item in the tree on the far left
-    def _tree(self, dir: Folder, depth: int = 0, last: bool = True) -> None:
-        indent: str = "    " * depth + "├── "
-        indent_final : str = "    " * depth + "└── "
 
-        if len(dir.list()) == 0: color_file = "[green]"
-        else: color_file = "[bold green]"
-
-        if depth == 0: self.sys.io.display.print("[blue]" + indent_final + "["+ dir.hex_addr + "] " + dir.name + "[/]")
-        
+    def tree(self, dir: Folder,tree : Tree = Tree(""), depth : int = 0):
+        if depth == 0:
+            tree = Tree("[blue]" + "["+ dir.hex_addr + "] " + dir.name + "[/]")
         else:
-            if last == True or len(dir.list()) == 1: 
-                self.sys.io.display.print(color_file + indent_final + "[/]" + "[" + dir.hex_addr + "] " + color_file + dir.name + "[/]")
-            else: 
-                self.sys.io.display.print( color_file + indent + "[/]" + "[" + dir.hex_addr + "] " + color_file + dir.name+ "[/]")
+            if len(dir.list()) == 0: color_file = "[green]"
+            else: color_file = "[bold green]"
+
+            tree = tree.add("[" + "[cyan]" + dir.hex_addr + "[/]" + "] " + color_file + dir.name + "[/]")
             
         for item in dir.list():   
-            if type(item) == Folder : self._tree(item, depth + 1, item == dir.list()[-1])
+            if type(item) == Folder : self.tree(item, tree, depth + 1)
             if type(item) ==  File:
-                if item == dir.list()[-1]: self.sys.io.display.print(f"    {indent_final}[{item.hex_addr}] {item.name}")
-                else: self.sys.io.display.print(f"    {indent}[{item.hex_addr}] {item.name}")
-
-
+                tree.add("[" +  "[cyan]" + dir.hex_addr + "[/]" + "] " + item.name)
+        return tree
+    def _tree(self, dir: Folder):
+        self.sys.io.display.print(self.tree(dir))
